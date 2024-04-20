8bb9d59f6f1a12fed1c4967cd5e8e4619eb302c9
charmaine211 <charmaine211@hotmail.com>
2024-04-20 13:56:02 +0200

    Removed building block

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import test from "./config";
 
-import SupportFormatText from "./components/atoms/SupportFormatText";
-
 function App() {
   const [data, setData] = useState("");
 
@@ -20,7 +18,6 @@ function App() {
   return (
     <div>
       {typeof data === "undefined" ? <p>LOADING...</p> : data}
-      <SupportFormatText formats = {[ "bmp", "dng", "jpeg"]}/>
     </div>
   );
 }
