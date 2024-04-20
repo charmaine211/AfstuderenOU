15a1baeb7f88c301e94d9b6d1e694d95ab32d6f8
charmaine211 <charmaine211@hotmail.com>
2024-04-20 14:49:49 +0200

    Made start with theme

---
+++ client/src/constants/theme.jsx
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+export const THEME  = createTheme({
+    palette: {
+        primary: {
+          main: "#48A3C0",
+        },
+        secondary: {
+          main: "#48A3C0",
+        },
+        info: {
+            main: "#48A3C0",
+          },
+          accent: {
+            main: "#48A3C0",
+          },
+      },
+  });
