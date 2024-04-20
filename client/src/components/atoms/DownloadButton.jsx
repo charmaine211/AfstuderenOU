520b8e4da97cf252d56b71bcde58cb57334024ea
charmaine211 <charmaine211@hotmail.com>
2024-04-20 14:53:22 +0200

    Added download button

---
+++ client/src/components/atoms/DownloadButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function DownloadButton () {
+    return (
+        <Button variant="contained">Download</Button>
+    );
+}
+
+export default DownloadButton;
