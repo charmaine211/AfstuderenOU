2e97b4693129528b2af16fe77d58ecfc138344e6
Charmaine <54951086+charmaine211@users.noreply.github.com>
2024-04-22 14:34:34 +0200

    Added all components for Footer

---
+++ client/src/components/atoms/FooterButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function FooterButton ({ text, onClick }) {
+
+    function handleOnClick(){
+        onClick();
+    }
+
+    return (
+        <Button variant="text" onClick={handleOnClick}>{ text }</Button>
+    );
+}
+
+FooterButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+}
+
+export default FooterButton;
