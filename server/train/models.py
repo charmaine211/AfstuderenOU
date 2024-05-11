8253c3a3b3c5a648e345d726b230d942f049a0ed
charmaine211 <charmaine211@hotmail.com>
2024-05-11 17:33:27 +0200

    Face detection works, constants module not yet

--- server/train/models.py
+++ server/train/models.py
@@ -5,7 +5,10 @@ import csv
 from pathlib import Path
 from ultralytics import YOLO
 
-MODEL_PATH = "../models/image_classicification--epochs_25-batchsize_all_last.pt"
+from constants import CLASSES
+
+MODEL_PATH = "../server/models/yolov8n-face.pt"
+FACE_MODEL_PATH = "../server/models/yolov8n-face.pt"
 IMAGE_DIRS = [
     "C:/Users/charm/Documents/Informatica/Postpropodeuse/Voorbereiden Afstuderen/Dataset/recording_10042024_Wim_v1/dataset/image_classification/test/dashboard_down_right",
     "C:/Users/charm/Documents/Informatica/Postpropodeuse/Voorbereiden Afstuderen/Dataset/recording_10042024_Wim_v1/dataset/image_classification/test/dashboard_straight_down",
@@ -21,6 +24,7 @@ IMAGE_DIRS = [
 ]
 IMAGE_DIR = "C:/Users/charm/Documents/Informatica/Postpropodeuse/Voorbereiden Afstuderen/Dataset/recording_10042024_Wim_v1/dataset/image_classification/test/dashboard_down_right"
 RESULTS_DIR = "../dataset"
+FILE_DIR = "../dataset"
 
 
 def predict_image_classification(path_model, image_file):
@@ -47,15 +51,18 @@ def predict_object_detection(path_model, image_file):
     model = YOLO(path_model)
     predictions = {}
     predictions["filename"] = image_file
+    txt_file = os.path.basename(image_file).split(".")[0]
     print("Model successfully uploaded")
     results = model(image_file)
     print("Results gathered")
     for result in results:
         prediction = {}
         i = 0
-        for box in result.box:
+        result.save_txt(txt_file)
+        # result.show()
+        for box in result.boxes:
             print(f"{result.names[i]}:{box.xywhn}\n")
-            prediction[result.names[i]] = box.xywh
+            prediction[result.names[i]] = box.xywhn
             i += 1
         predictions["prediction"] = prediction
 
@@ -82,14 +89,40 @@ def create_csv(data, title):
     print("CSV succesfully created")
 
 
-def main():
-    print("***PREDICTION***")
+def label_frame(image_file, label_id, result_dir):
+    model = YOLO(FACE_MODEL_PATH)
+    txt_file = os.path.basename(image_file).split(".")[0]
+    results = model(image_file)
+    results_path = os.path.join(result_dir, txt_file)
+
+    for result in results:
+        result.save_txt(results_path)
+
+    with open(results_path, "r+") as file:
+        first_line = file.readline().strip().split()
+        first_line[0] = str(label_id)
+        file.seek(0)
+        file.write(" ".join(first_line))
+        file.truncate()
+
+
+def label_dataset():
     for image_dir in IMAGE_DIRS:
+        label_id = CLASSES.index(os.path.basename(image_dir))
+        print(label_id)
+        break
 
-        for image_path in glob.glob(f"{image_dir}/*"):
-            prediction = predict_image_classification(MODEL_PATH, image_path)
 
-            create_csv(prediction, os.path.basename(MODEL_PATH))
+def main():
+
+    print("***PREDICTION***")
+    label_dataset()
+    # for image_dir in IMAGE_DIRS:
+
+    #     for image_path in glob.glob(f"{image_dir}/*"):
+    #         label_frame(image_path, 10, RESULTS_DIR)
+    # prediction = predict_object_detection(MODEL_PATH, image_path)
+    # create_csv(prediction, os.path.basename(MODEL_PATH))
 
     # model = YOLO(MODEL_PATH)
     # f = open(f"{FILE_DIR}/test_results.txt", "a")
@@ -106,7 +139,7 @@ def main():
     #         f.write(f"{classes}")
     #         i = 0
     #         for prob in result.probs:
-    #             f.write(f"{result.names[i]}:{prob.data}\n")
+    #             #  f.write(f"{result.names[i]}:{prob.data}\n")
     #             i += 1
 
     # f.close()
