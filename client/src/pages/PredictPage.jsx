b5a0c524f59c3b4503a05275afbadb1c3442a42a
charmaine211 <charmaine211@hotmail.com>
2024-06-09 16:16:50 +0200

    Placed input in middle container

--- client/src/pages/PredictPage.jsx
+++ client/src/pages/PredictPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import { Container } from '@mui/material';
+
 import { predictAnalysis } from '../common/api/predict';
 
 import PageWrapper from '../components/organisms/PageWrapper';
@@ -44,26 +46,40 @@ function PredictPage () {
             {!modelIsUploaded && 
             <>
                 <TitleTextBlock
-                title="Upload model"
-                text="Upload a trained Ultralytics YOLO model.
-                If you don't have a trained model, download the instructions here."/>
-                <InputModel
-                    onUploaded={ handleModelUpload} />
+                    title="Upload model"
+                    text="Upload a trained Ultralytics YOLO model.
+                    If you don't have a trained model, download the instructions here."/>
+                <Container 
+                maxWidth="sm" 
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    marginTop: "5em",
+                }}>
+                    <InputModel
+                        onUploaded={ handleModelUpload} />
+                </Container>
             </>}
             {modelIsUploaded && !avIsUploaded && 
             <>
                 <TitleTextBlock
                 title='Upload  images and/or videos'
                 text='Upload video and/or image files of drivers for analysis by the model.'/>
-                <InputAV  
-                    onUploaded={ handleAvUpload } />
+                <Container>
+                    <InputAV  
+                        onUploaded={ handleAvUpload } />
+                </Container>
             </>} 
             {modelIsUploaded && avIsUploaded && 
             <>
                 <TitleTextBlock
                 title='Analysis'
                 text='Results of the analysis of your files'/>
-                <AnalysesResults />
+                <Container>
+                    <AnalysesResults />
+                </Container>
             </>}
         </PageWrapper>
     
