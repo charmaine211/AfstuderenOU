460be789d1dc274c3348962eb17f2096ae2ac873
charmaine211 <charmaine211@hotmail.com>
2024-06-28 13:04:13 +0200

    Prepare train page

--- client/src/pages/TrainPage.jsx
+++ client/src/pages/TrainPage.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import PageWrapper from '../components/organisms/PageWrapper';
 
-import GoogleDrive from '../components/molecules/GoogleDrive';
-import GoogleColab from '../components/molecules/GoogleColab';
-import LightningAI from '../components/molecules/LightningAI';
+import { Paper } from "@mui/material"
+
+import PageWrapper from '../components/organisms/PageWrapper';
 
 function TrainPage () {
     return (
     <PageWrapper>
-        <h1>Train</h1>  
+        <h1>Train</h1> 
 
-        <GoogleDrive/>
-        <GoogleColab/>
-        <LightningAI/>
+
     </PageWrapper>
     
     );
