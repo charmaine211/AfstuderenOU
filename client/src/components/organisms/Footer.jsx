813c687a7f7f6861f31316dc26484f442e98de9e
charmaine211 <charmaine211@hotmail.com>
2024-07-26 14:20:26 +0200

    Added Github info, easy to change

--- client/src/components/organisms/Footer.jsx
+++ client/src/components/organisms/Footer.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import FooterButton from '../atoms/FooterButton';
 
 function Footer () {
+
+    const handleGetDocumentation = () => {
+        window.open("https://github.com/charmaine211/Automatic_Driver_Gaze_Detection/blob/main/README.md");
+    };
+
+    const handeGetPrivacyPolicy = () => {
+        window.open("https://github.com/charmaine211/Automatic_Driver_Gaze_Detection/blob/main/PRIVACY_POLICY.md")
+    };
+
+    // handleContact = () => {
+    //     alert("TODO Contact form")
+    // }
+
     return (
         <footer>
             <FooterButton 
-                text="Privacy Policy" onClick={()=>{ alert("TODO Privacy policy")}}/>
+                text="Privacy Policy" onClick={handeGetPrivacyPolicy}/>
+            {/* <FooterButton 
+                text="Contact us" onClick={()=>{ }}/> */}
             <FooterButton 
-                text="Contact us" onClick={()=>{ alert("TODO Contact form")}}/>
+                text="Instructions" onClick={handleGetDocumentation}/>
         </footer>
     );
 };
