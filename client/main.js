28032fcd35051e517580baf118b5a0437c25aba0
charmaine211 <charmaine211@hotmail.com>
2024-08-02 19:33:58 +0200

    NOT FINISHED - just to push

--- client/main.js
+++ client/main.js
@@ -7,64 +7,76 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
     height: 600,
-    icon: __dirname + '/client/assets/brain-background.png',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: true,
-      preload: path.join(__dirname, "/public/preload.js"),
-    }  
+      preload: app.isPackaged ? path.join(__dirname, './build/preload.js') : path.join(__dirname, './public/preload.js'),
+    }
   });
+
+  // const ASSETS_PATH = app.isPackaged ?
+  //   path.join(process.resourcesPath, 'assets') : "../../assets";
 
   const appURL = app.isPackaged
-  ? url.format({
-      pathname: path.join(`${__dirname}/public/index.html`),
-      protocol: "file:",
-      slashes: true,
-    })
-  : "http://localhost:3000";
+    ? url.format({
+        pathname: path.join(__dirname, './build/index.html'),
+        protocol: 'file:',
+        slashes: true,
+      })
+    : 'http://localhost:3000';
 
-  console.log(`Loading app: ${appUrl}`);
+  console.log(`Loading app: ${appURL}`);
 
-  win.loadURL(appURL).catch((err) => {
-    console.error('Failed to load app:', err);
-  });
+  setTimeout(() => {
+    win.loadURL(appURL).catch((err) => {
+      console.error('Failed to load app:', err);
+    });
+  
+  }, 2000);
 
   // Open the DevTools if needed
   win.webContents.openDevTools();
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+function startFlaskApp() {
+  // const flaskPath = app.isPackaged ? path.join(__dirname, './build/resources/app') : path.join(__dirname, 'resources', 'app');
+  const flaskPath = path.join(__dirname, './resources/app');
+  flaskProcess = execFile(flaskPath, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error starting Flask app: ${error.message}`);
+      return;
+    }
+    console.log(`Flask app output: ${stdout}`);
+  });
+}
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+app.whenReady().then(() => {
+  createWindow();
+  startFlaskApp();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+function killPython() {
+  const kill = require("tree-kill");
+  kill(flaskProcess.pid);
+}
+
+// Quit when all windows are closed, except on macOS.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 });
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+app.on('will-quit', () => {
+  if (flaskProcess) {
+    killPython();
   }
 });
 
-// ipcMain.handle('select-file', async (event) => {
-//   const mainWindow = BrowserWindow.fromWebContents(event.sender);
-//   const result = await dialog.showOpenDialog(mainWindow, {
-//     properties: ['openFile'],
-//   });
-
-//   if (!result.canceled) {
-//     return result.filePaths[0];
-//   } else {
-//     return null;
-//   }
-// });
+// export default ASSETS_PATH;
